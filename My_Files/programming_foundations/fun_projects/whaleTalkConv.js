580699f2c923ca1f6ec02d3e66c2f775112f3983
Glenn Key <glennkey@Glenns-MacBook-Pro.local>
2019-05-26 17:39:37 +0200

    created new whale-human converter

---
+++ My_Files/programming_foundations/fun_projects/whaleTalkConv.js
@@ -0,0 +1,20 @@
+const input = 'To be, or not to be. That is the question.';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const resultArray = [];
+for (let i = 0; i < input.length; i++) {
+  for (let v = 0; v < vowels.length; v++){
+    if (input[i] === vowels[v]){
+        if (input[i] === 'e'){
+       resultArray.push('ee');
+     } else if (input[i] === 'u'){
+       resultArray.push('uu');
+     }
+      else {
+       resultArray.push(input[i]);
+     }
+    } 
+  }
+}
+
+  
+console.log(resultArray.join('').toUpperCase());
